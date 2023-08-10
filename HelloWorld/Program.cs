0dc38289bb929b80e14148813dcb9dfd2143d738
thiag <thiag@LAPTOP-I1GJ06OO>
2023-08-09 21:26:14 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Meu nome é Thiago Lins");
+
+
+            Console.ReadLine();        
+        }
+    }
+}
